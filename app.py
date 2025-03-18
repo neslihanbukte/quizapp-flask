de117ebd674c1e37b9c5c160be9776ac5fe51797
neslihan bükte <neslihanbukte20@gmail.com>
2025-03-18 18:41:34 +0300

    folders updated

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///quiz.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/esraarslan/quizapp-flask/quiz.db'
 db = SQLAlchemy(app)
 
 class User(db.Model):
